e9cb4e4037d8aedda2c38c06434ab1df75cdd111
Harsh Verma <harshkardam246@gmail.com>
2025-03-18 20:18:09 +0530

    commit 2/n

--- Android Application/app/src/main/java/com/example/ayurveda/MainActivity.kt
+++ Android Application/app/src/main/java/com/example/ayurveda/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ayurveda.ui.theme.AyurvedaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +24,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             AyurvedaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AyurvedaTheme {
-        Greeting("Android")
+fun MainScreen(modifier: Modifier = Modifier){
+    Row(modifier = Modifier.padding(50.dp).align(Alignment.Center),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(text = "Ayurveda App")
+
     }
 }
+
+private fun Modifier.align(alignment: Alignment): Modifier {}
